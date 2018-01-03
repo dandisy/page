db150aa7475b379bfa8d99a8fe1c91e82a9b2276
dandi <dandi.webdev@gmail.com>
2018-01-03 07:42:37 +0700

    add menu-manager route

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,5 @@
 <?php
 
 Route::get('/{uri}/{all?}', 'Webcore\Page\PageController@index')
-    ->where('uri', '(?!img)(?!roles)(?!users)(?!settings)(?!dashboard)(?!assets)(?!admin)(?!register$)(?!login$)(?!logout$)([A-Za-z0-9\-]+)')
+    ->where('uri', '(?!img)(?!menu-manager)(?!roles)(?!users)(?!settings)(?!dashboard)(?!assets)(?!admin)(?!register$)(?!login$)(?!logout$)([A-Za-z0-9\-]+)')
     ->where('all', '.*');
