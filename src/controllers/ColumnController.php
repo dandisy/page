bab1ced2a10a5635b885e83fd358fa836e25a720
dandi <dandi.webdev@gmail.com>
2018-03-13 01:22:36 +0700

    fixing

--- src/controllers/ColumnController.php
+++ src/controllers/ColumnController.php
@@ -2,6 +2,7 @@
 
 namespace Webcore\Page;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 Route::get('/{uri}/{all?}', 'Webcore\Page\PageController@index')
-    ->where('uri', config('webcore.page-system.skip', '').'(?!roles|users|settings|admin|register|login|logout)([A-Za-z0-9\-]+)')
+    ->where('uri', config('webcore.page-system.skip', '').'(?!roles|users|settings|admin|register|login|logout|getDataTable|getTableColumn)([A-Za-z0-9\-]+)')
     ->where('all', '.*');
 
 Route::get('/getDataTable', 'Webcore\Page\PageController@getDataTable');
