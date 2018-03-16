27ca3f37dbb3c1df341c99f348065ac9d21adec6
dandi <dandi.webdev@gmail.com>
2018-03-16 15:08:23 +0700

    fixing to csrf token

--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,10 @@
 <?php
 
-Route::get('/{uri}/{all?}', 'Webcore\Page\PageController@index')
-    ->where('uri', config('webcore.page-system.skip', '').'(?!roles|users|settings|admin|register|login|logout|getDataTable|getTableColumn)([A-Za-z0-9\-]+)')
-    ->where('all', '.*');
+Route::group(['middleware' => 'web'], function () {
+    Route::get('/{uri}/{all?}', 'Webcore\Page\PageController@index')
+        ->where('uri', config('webcore.page-system.skip', '').'(?!roles|users|settings|admin|register|login|logout|getDataTable|getTableColumn)([A-Za-z0-9\-]+)')
+        ->where('all', '.*');
+});
 
 Route::get('/getDataTable', 'Webcore\Page\PageController@getDataTable');
 
